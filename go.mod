10c2770d3935dccf494064bfb8db0ef6de398e60
wertasy <wertasy@qq.com>
2021-08-19 23:55:40 +0800

    lzma

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module canhui.wang/factory
 
 go 1.16
+
+require github.com/ulikunitz/xz v0.5.10 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,63 @@
 package main
 
 import (
-	"canhui.wang/factory/boot"
+	"archive/tar"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"path"
+
+	"github.com/ulikunitz/xz/lzma"
 )
 
 func main() {
-	boot.PowerOn()
+	// boot.PowerOn()
+	baseDir := "tmp"
+	file, err := os.Open("v2fly-core.tar.lzma")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	lr, err := lzma.NewReader(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tr := tar.NewReader(lr)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		path := path.Join(baseDir, hdr.Name)
+		mode := hdr.FileInfo().Mode()
+		foo(tr, path, mode)
+	}
+}
+
+func foo(src io.Reader, path string, mode fs.FileMode) {
+	if mode.IsDir() {
+		err := os.MkdirAll(path, mode.Perm())
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if mode.IsRegular() {
+		dst, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, mode.Perm())
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer dst.Close()
+
+		if _, err := io.Copy(dst, src); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
